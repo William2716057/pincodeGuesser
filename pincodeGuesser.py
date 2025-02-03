b8d47e74d6050170094a7f19afae51112f1b8393
User <Willspri@gmail.com>
2025-02-03 17:48:04 +1300

    improved repeat function added

--- pincodeGuesser.py
+++ pincodeGuesser.py
@@ -4,17 +4,23 @@ knowns = input("Enter numbers: ")
 
 numbers = list(map(int, knowns.split()))
 
+
 possibilities = permutations(numbers)
 
 for i in list(possibilities):
     print(i)
-
-#know that 1234 have been pressed but code requires 5 numbers 
-#output all permutations where each number repeats once 
-
-numbers.append(numbers[0])  
-
-possibilities2 = set(permutations(numbers))
-
-for i in possibilities2:
-    print(i)
+    
+#iterations
+for index in range(len(numbers)):
+    #Copy numbers 
+    modified = numbers.copy()
+    # Append each number
+    modified.append(modified[index])
+    
+    # Generate permutations using modified
+    possibilities = set(permutations(modified))
+    
+    print(f"Possibilities with {modified[index]}: ")
+    for i in possibilities:
+        print(i)
+    print() 
